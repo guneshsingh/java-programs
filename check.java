304f45b16cc65a598c21fdc591242f275ac9afa2
guneshsingh30 <guneshsingh30@gmail.com>
2026-09-18 00:26:04 +0530

    main

---
+++ check.java
@@ -0,0 +1,14 @@
+class check{
+    public static void main(String[] args) {
+        int n=5;
+        if(n>0){
+            System.out.println("pso");
+        }
+        else if(n<0){
+            System.out.println("ne");
+        }
+        else{
+            System.out.println("zero");
+        }
+    }
+}
